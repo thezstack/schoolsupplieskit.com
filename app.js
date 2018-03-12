e622ac7921a1d60fb69b196f85b63625ddea47b1
Zubair Mughal <mghlzbr@gmail.com>
2018-03-11 19:52:21 -0500

    shopping cart model working

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const stripe = require('stripe')('keys.stripeSecretKey');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const app = express();
-
+const csrf = require('csurf');
+const csrfProtection = csrf();
 const port = process.env.PORT || 5000;
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 // Mongo
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/hqa-products');
 
 // Model
 const product = require('./models/product');
-
+const Cart = require('./models/cart');
 //Handlebars Middleware
 
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
@@ -23,35 +26,71 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(session({
+  secret:'mysupersecret',
+  resave:false,
+  saveUninitialized:false,
+  store:new MongoStore({mongooseConnection:mongoose.connection}),
+  cookie:{maxAge:180 * 60 * 1000}
+  }));
+  app.use(function(req, res, next) {
+      res.locals.session = req.session;
+      next();
+  });
 //Set Static Folder
-
 app.use(express.static(`${__dirname}/public`));
 
 
 // Index Route
 app.get('/', (req, res) => {
-  res.render('index'), {
+  res.render('index')
+});
 
-    stripePublishableKey: keys.stripePublishableKey
-  }
+// Store
+app.get('/add-to-cart/:id', (req,res) => {
+    const productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {items:{}});
+
+    product.findById(productId, function(err, product) {
+       if (err) {
+           return res.redirect('/');
+       }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+         res.redirect('/hqa');
+    });
+});
+
+app.get('/reduce/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    cart.reduceByOne(productId);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
+
+app.get('/remove/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    cart.removeItem(productId);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
 
 
-})
-//Success Route
-app.get('/success', (req, res) => {
-  res.render('success'), {
-  }
+app.get('/shopping-cart', function(req, res, next) {
+   if (!req.session.cart) {
+       return res.render('shopping-cart', {products: null});
+   }
+    var cart = new Cart(req.session.cart);
+    res.render('shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
 
 
-})
-// Contact Route
-app.get('/contact', (req, res) => {
-  res.render('contact'), {
-  }
-
-//HQA Store Front
-})
+//HQA view
 
 
 app.get('/hqa', (req, res) => {
@@ -66,7 +105,10 @@ app.get('/hqa', (req, res) => {
     });
   });
 
-
+//Contact
+app.get ('/contact', (req,res) => {
+  res.render('contact');
+})
 
 //Charge Route
 app.post('/charge', (req, res) => {
